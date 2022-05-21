283def586815caab37b9745cec61329b6079f4f6
Gaurav Narayan Naik <57282515+Gaurav-Naik7@users.noreply.github.com>
2022-05-21 05:00:21 +0200

    Add files via upload

---
+++ program115.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+int main()
+{
+	// Static memory allocation
+	NODE obj;
+	
+	//Dynamic memory allocation
+	PNODE ptr=(PNODE)malloc(sizeof(NODE));
+	
+	obj.data =11;  //Direct accessing operator .
+	obj.next =NULL;
+	
+	ptr->data=11;  //Indirect accessing operator ->
+	ptr->next=NULL;
+	
+	return 0;
+}
